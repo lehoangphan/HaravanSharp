0fc70445873a3a844923396c11c40d6ae513287c
lehoangphan <itphan1997@gmail.com>
2020-12-27 03:18:22 +0700

    clean project

--- HaravanSharp/BaseHaravanRepository.cs
+++ HaravanSharp/BaseHaravanRepository.cs
@@ -8,7 +8,7 @@ namespace HaravanData.Repositories
 {
     public abstract class BaseHaravanRepository
     {
-       protected virtual string Jwtoken { get; set; }
+       protected virtual string JWToken { get; set; }
        protected virtual string BaseApiUrl { get; set; }
     }
 }
