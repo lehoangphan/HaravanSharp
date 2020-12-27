6ddc06342d14d5f3c205ad2927815c4aa8b61d33
lehoangphan <itphan1997@gmail.com>
2020-12-27 10:55:28 +0700

    delete .vs folder

--- HaravanSharp/BaseHaravanRepository.cs
+++ HaravanSharp/BaseHaravanRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-
-namespace HaravanData.Repositories
+﻿namespace HaravanData.Repositories
 {
     public abstract class BaseHaravanRepository
     {
